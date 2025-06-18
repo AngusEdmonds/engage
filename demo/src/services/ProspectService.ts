00b771940ef80db7da800aa8663b240286b5c2d2
Angus Edmonds <angusedmonds@giantdigitalmarketing.com>
2025-06-19 02:03:45 +1000

    prospect service fix

--- demo/src/services/ProspectService.ts
+++ demo/src/services/ProspectService.ts
@@ -1,8 +1,7 @@
-import appConfig from '@/configs/app.config'
-import BaseService from './BaseService'
+import BaseService from '@/services/BaseService'
 
 const Api = async function axiosApi<T>(url: string, method: string, data?: unknown): Promise<T> {
-    return BaseService.fetchData<T>({
+    return BaseService.request<T>({
         url,
         method,
         data,
@@ -11,18 +10,15 @@ const Api = async function axiosApi<T>(url: string, method: string, data?: unkno
 
 // 🚀 GET Prospect List
 export async function apiGetAllProspects<T, U extends Record<string, unknown>>(data: U) {
-    console.log('🚀 Calling GET prospects list API', data)
     return Api<T>('/prospects/list', 'post', data)
 }
 
 // 🚀 GET Prospect Statistics
 export async function apiGetAllProspectsStatistic<T>() {
-    console.log('🚀 Calling GET prospect statistics API')
     return Api<T>('/prospects/statistics', 'get')
 }
 
 // 🚀 PUT Update Prospect
 export async function apiPutProspect<T, U extends Record<string, unknown>>(data: U) {
-    console.log('🚀 Calling PUT update prospect API', data)
     return Api<T>('/prospects/update', 'put', data)
 }
