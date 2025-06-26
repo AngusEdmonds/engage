f7295978c04e383ac4223ca746414a8e134c0e9f
Angus Edmonds <angusedmonds@giantdigitalmarketing.com>
2025-06-27 05:42:05 +1000

    update

--- demo/src/views/sales/SalesDashboard/components/SalesDashboardBody.tsx
+++ demo/src/views/sales/SalesDashboard/components/SalesDashboardBody.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '@/store'
 import { getSalesDashboardData } from '@/views/sales/SalesDashboard/store/salesDashboardSlice'
+
 import Statistic from './Statistic'
 import TopProduct from './TopProduct'
 import SalesReport from './SalesReport'
@@ -9,6 +10,7 @@ import LatestOrder from './LatestOrder'
 
 const SalesDashboardBody = () => {
     const dispatch = useAppDispatch()
+
     const data = useAppSelector((state) => state.salesDashboard?.data)
     const loading = useAppSelector((state) => state.salesDashboard?.loading)
 
@@ -20,19 +22,13 @@ const SalesDashboardBody = () => {
         return <div className="p-6">Loading dashboard...</div>
     }
 
-    const statistics = data.statistics ?? []
-    const topProducts = data.topProduct ?? []
-    const salesReport = data.salesReport ?? []
-    const categories = data.salesByCategories ?? []
-    const latestOrders = data.latestOrder ?? []
-
     return (
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
-            <Statistic data={statistics} />
-            <TopProduct data={topProducts} />
-            <SalesReport data={salesReport} />
-            <SalesByCategories data={categories} />
-            <LatestOrder data={latestOrders} />
+            <Statistic data={data.statistics ?? []} />
+            <TopProduct data={data.topProduct ?? []} />
+            <SalesReport data={data.salesReport ?? []} />
+            <SalesByCategories data={data.salesByCategories ?? []} />
+            <LatestOrder data={data.latestOrder ?? []} />
         </div>
     )
 }
